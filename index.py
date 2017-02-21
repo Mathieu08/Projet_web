b73b84acd4a104a992f9e319eb0d69adf8984939
Mathieu <mat.caouette08@gmail.com>
2017-02-20 23:00:58 -0500

    Communication with database added and templates implementation.

--- index.py
+++ index.py
@@ -5,9 +5,29 @@ from flask import render_template
 from flask import g
 from flask import request
 from flask import redirect
+from database import Database
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+def get_database():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
+    
 
 @app.route('/')
 def accueil():
-	return render_template('accueil.html')
+    articles = get_database().get_articles()
+    return render_template('accueil.html', articles=articles)
+
+@app.route('/search', methods=['POST'])
+def recherche():
+    articles = get_database().get_articles_search(request.form['search'])
+    return render_template('recherche.html', articles=articles)
